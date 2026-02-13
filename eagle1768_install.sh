8d358aee63171a2d2008a047fbcec72cf751ce52
ShaheenHossain <rapidgrps@princegroup-bd.com>
2026-02-13 23:22:15 +0600

    Modify pip install command for requirements
    Updated pip install command to include --break-system-packages option.

--- eagle1768_install.sh
+++ eagle1768_install.sh
@@ -65,7 +65,7 @@ sudo apt-get install git python3-cffi build-essential wget python3-dev python3-v
 
 echo -e "\n---- Install python packages/requirements ----"
 #sudo -H pip3 install -r https://github.com/odoo/odoo/raw/17.0/requirements.txt
-sudo -H pip3 install -r https://github.com/ShaheenHossain/requirements.txt/raw/master/requirements.txt
+sudo -H pip3 install -r https://github.com/ShaheenHossain/requirements.txt/raw/master/requirements.txt --break-system-packages
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
